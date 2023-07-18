e146ee3b3b9667f18420ee8d7f85f2bc6f046f42
nazzal ramzan <nazzalra@gmail.com>
2023-07-18 21:21:49 +0700

    Search by Country

--- tests/Feature/PropertySearchTest.php
+++ tests/Feature/PropertySearchTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\City;
+use App\Models\Country;
 use App\Models\Property;
 use App\Models\Role;
 use App\Models\User;
@@ -26,5 +27,24 @@ class PropertySearchTest extends TestCase
         $response->assertStatus(200);
         $response->assertJsonCount(1);
         $response->assertJsonFragment(['id' => $propertyInCity->id]);
+    }
+
+    public function test_search_property_by_country()
+    {
+        $owner = User::factory()->create()->assignRole(Role::ROLE_OWNER);
+        $countries = Country::with('cities')->take(2)->get();
+        $propertyInCountry = Property::factory()->create([
+            'owner_id' => $owner->id,
+            'city_id' => $countries[0]->cities()->value('id')
+        ]);
+        $propertyInAnotherCountry = Property::factory()->create([
+            'owner_id' => $owner->id,
+            'city_id' => $countries[1]->cities()->value('id')
+        ]);
+        
+        $response = $this->getJson('/api/search?country=' . $countries[0]->id);
+        $response->assertStatus(200);
+        $response->assertJsonCount(1);
+        $response->assertJsonFragment(['id' => $propertyInCountry->id]);
     }
 }
