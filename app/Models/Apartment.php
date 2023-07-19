1ae085dfb3c1d2820ecb43d0c9bd7f104e037948
nazzal ramzan <nazzalra@gmail.com>
2023-07-19 21:49:15 +0700

    Apartment Rooms and Beds: DB Structure

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Apartment extends Model
 {
@@ -16,7 +17,8 @@ class Apartment extends Model
         'name',
         'capacity_adults',
         'capacity_children',
-        'size'
+        'size',
+        'bathrooms'
     ];
 
     public function property(): BelongsTo
@@ -27,5 +29,10 @@ class Apartment extends Model
     public function apartment_type(): BelongsTo
     {
         return $this->belongsTo(ApartmentType::class);
+    }
+
+    public function rooms(): HasMany
+    {
+        return $this->hasMany(Room::class);
     }
 }
