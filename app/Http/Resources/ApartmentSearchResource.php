0da6217717a95fcd4ba95404ef263c8b8bc43c56
nazzal ramzan <nazzalra@gmail.com>
2023-07-24 06:49:28 +0700

    Fix whenLoaded issue

--- app/Http/Resources/ApartmentSearchResource.php
+++ app/Http/Resources/ApartmentSearchResource.php
@@ -20,8 +20,7 @@ class ApartmentSearchResource extends JsonResource
             'size' => $this->size,
             'beds_list' => $this->beds_list,
             'bathrooms' => $this->bathrooms,
-            // 'facilities' => FacilityResource::collection($this->whenLoaded('facilities')),
-            'facilities' => FacilityResource::collection($this->facilities)
+            'facilities' => FacilityResource::collection($this->whenLoaded('facilities')),
         ];
     }
 }
